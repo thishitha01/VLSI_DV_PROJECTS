7b24c31f28871ce203c697fb403b6e04229bc926
Govini Thishitha <gthishitha@gmail.com>
2026-08-08 22:12:36 +0530

    Create package.sv

---
+++ APB_SLAVE_VERIFICATION_UVM/package.sv
@@ -0,0 +1,15 @@
+`include "interface.sv"
+package pkg;
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+`include "transaction.sv"
+`include "sequence.sv"
+`include "sequencer.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "agent.sv"
+`include "scoreboard.sv"
+`include "coverage.sv"
+`include "environment.sv"
+`include "tests.sv"
+endpackage
